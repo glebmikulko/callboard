52c35efedef87d500e1b9e902fe6d97bd458ce03
Gleb Mikulko <glebmikulko@gmail.com>
2016-09-29 22:52:55 +0300

    Add category and categories&posts seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# this file should contain all the record creation needed to seed the database with its default values.
+# the data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
+# examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = movie.create([{ name: 'star wars' }, { name: 'lord of the rings' }])
+#   character.create(name: 'luke', movie: movies.first)
 
-10.times do
-  post_text = Faker::Hacker.noun
-  Post.create(title: post_text, body: Faker::Lorem.paragraphs(rand(0..4)),
-              price: Faker::Commerce.price, status: [:on_moderation, :accepted, :rejected].sample,
-              image: Faker::Placeholdit.image("50x50", 'jpg', 'ffffff', '000', post_text))
+# 50.times do
+#   post_text = faker::hacker.noun
+#   post.create(title: post_text, body: faker::lorem.paragraphs(rand(0..4)),
+#               price: faker::commerce.price, status: [:on_moderation, :accepted, :rejected].sample,
+#               image: faker::placeholdit.image("50x50", 'jpg', 'ffffff', '000', post_text))
+# end
+#
+# 10.times do 
+#   category.create(name: faker::music.instrument)
+# end
+
+Post.all.each do |post|
+    category = Category.all.sample
+    category.posts << post
 end
