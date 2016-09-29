65823983d57662f994a9119de30c42eab1c84cc9
Gleb Mikulko <glebmikulko@gmail.com>
2016-09-29 16:52:32 +0300

    Add post seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  post_text = Faker::Hacker.noun
+  Post.create(title: post_text, body: Faker::Lorem.paragraphs(rand(0..4)),
+              price: Faker::Commerce.price, status: [:on_moderation, :accepted, :rejected].sample,
+              image: Faker::Placeholdit.image("50x50", 'jpg', 'ffffff', '000', post_text))
+end
