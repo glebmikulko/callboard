cf15ba89e06ff1c9e0ece4f9fc52f2972b4dbdd9
Gleb Mikulko <glebmikulko@gmail.com>
2016-09-29 16:46:28 +0300

    Add image to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929125941) do
+ActiveRecord::Schema.define(version: 20160929134233) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160929125941) do
     t.integer  "status"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image"
   end
 
 end
