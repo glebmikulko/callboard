17204bb51690fb4e4b2e9bb69574f8e57d531fe4
Gleb Mikulko <glebmikulko@gmail.com>
2016-10-12 13:37:35 +0300

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   enum status: {on_moderation: 0, accepted: 1, rejected: 2}
   has_and_belongs_to_many :categories
-  validates :categories, presence: true
+  validates :categories, :status, presence: true
 end
